9edca8074f0fe905364758a1cb9eec104b3feb8a
Vsevolod <vsevolod.goloviznin@gmail.com>
2019-05-15 15:24:28 +0300

    - removed babel polyfill that is not required

--- app/code/community/Datalay/FindifyFeed/Model/Observer.php
+++ app/code/community/Datalay/FindifyFeed/Model/Observer.php
@@ -4,13 +4,13 @@ class Datalay_FindifyFeed_Model_Observer extends Varien_Event_Observer
 {
     public function prepareLayoutBefore(Varien_Event_Observer $observer)
     {
-        $block = $observer->getEvent()->getBlock();
-        if ("head" == $block->getNameInLayout()) {
-	    $block->addExternalItem('external_js','//cdnjs.cloudflare.com/ajax/libs/babel-polyfill/6.23.0/polyfill.min.js');
+//         $block = $observer->getEvent()->getBlock();
+//         if ("head" == $block->getNameInLayout()) {
+// 	    $block->addExternalItem('external_js','//cdnjs.cloudflare.com/ajax/libs/babel-polyfill/6.23.0/polyfill.min.js');
 	    //$block->addJs('polyfill.min.js');
-        }else{
+//         }else{
             //Mage::log('getNameInLayout is not head. It is: '. $block->getNameInLayout());
-        }
+//         }
         return $this;
     }
 
